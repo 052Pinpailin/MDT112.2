77162d4375bdcd3161ed93d0b10930280e0c8bb3
Pinpailin <pinpailin3146@gmail.com>
2023-03-13 17:31:15 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("Please input Day: ");
+        bool Day = bool.Parse(Console.ReadLine());
+        Console.Write("Please input Time: ");
+        bool time = bool.Parse(Console.ReadLine());
+        Console.Write("Please input Food: ");
+        string Food = (Console.ReadLine());
+        
+        static bool(Day){
+        int 1 = Mon;
+        int 2 = Tue;
+        int 3 = Wed;
+        int 4 = Thu;
+        int 5 = Fri;
+        int 6 = Sat;
+        int 7 = Sun;
+        Console.WriteLine("Day is = {1 && 2 && 3 && 4 && 5 && 6 && 7}");
+        }
+        if(Time >= 8 && Time <= 18);
+
+        if((Food == "1")){
+            int 1 = Breakfast Set;
+            if(1 >= 5);{
+        }else {
+            Console.WriteLine("Sorry your order is out of stock");
+        }
+        }else if((Food == "2")){
+            int 2 = Weekend Set;
+            if(1 >= 2);{
+        }else {
+            Console.WriteLine("Sorry your order is out of stock");
+        }
+        }else if((Food == "3")){
+            int 3 = Coffee;
+            if(1 >= 3);{
+        }else {
+            Console.WriteLine("Sorry your order is out of stock");
+    }
+}
